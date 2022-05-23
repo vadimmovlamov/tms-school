ed7fd7c8271fa1634c35d16dd3469f6951aae64b
vadimmovlamov <vadim113512@tut.by>
2022-05-23 14:36:00 +0300

    add homework two

---
+++ hw_two/classwork.js
@@ -0,0 +1,109 @@
+/*  */
+const someFunction = (argument) => {
+    return argument();
+}
+const helloWorld = () => {
+    return "Hello world";
+}
+const sayGoodbye = () => {
+    return "Good bye"
+}
+console.log(someFunction(helloWorld));
+console.log(someFunction(sayGoodbye));
+// почему когда мы передаем someFunction ссылку на функцию helloWorld , argument() принимает ее и выводит в консоль?
+
+
+/* */
+const numbers = [1, 2, 3, 4, 5];
+const someFunction = (numberList) => {
+    const result = [];
+    for (const number of numberList) {
+        result.push(number * 2);
+    };
+    return result;
+}
+console.log(someFunction(numbers))
+const multiply = (numbers) => numbers.map((item, index) => {
+    console.log('item', item);
+    console.log('index', index);
+    return item * 2;
+})
+console.log(multiply(numbers))
+const multiply2 = (numbers) => numbers.map((item) => item * 2);
+console.log(multiply2(numbers))
+
+
+
+/* */
+const users = [{
+        name: 'alex',
+        age: 12,
+    },
+    {
+        name: 'oleg',
+        age: 25,
+    }
+
+]
+const mapUsers = (users) => {
+    return users.map((user, index) => {
+        return 1;
+    })
+}
+console.log(mapUsers(users)) * /
+
+/*  */
+const numbers = [1, 2, 3, 4, 5, 6];
+const filterEven = (numbersList) => {
+    return numbersList.filter((number) =>{
+        return (number % 2 === 0)
+    })
+}
+console.log(filterEven(numbers)) 
+
+
+
+/*  */
+const numbers = [1, 2, 3, 4, 5, 6];
+const someFunction = () => {
+    const result = [];
+    for (const number of numbers) {
+        result.push(number ** 2);
+    };
+    return result;
+}
+console.log(someFunction())
+const someFn = (numberList, degree) => {
+    return numberList.map((number) => Math.pow(number, degree))
+}
+console.log(someFn(numbers, 2))
+
+// const someFun = (numberList, degree) => numberList.map((number) => Math.pow(number, degree));
+// console.log(someFun(numbers, 2))
+
+
+/*  */
+const numbers = [1, 2, 3, 'Hello', 'lol'];
+const someFunc = (items) => {
+    return numbers.map(item => ({
+        value: item,
+        type: typeof item
+    }))
+}
+console.log(someFunc(numbers))
+
+/*  */
+const user = {
+    name: 'dsfsfsdf',
+    age: 12,
+    smth: [1, 2, 3, 'Hello', 'lol'],
+    a: 1,
+    b: 'asdasd'
+}
+const {
+    age,
+    a,
+    ...other
+} = user;
+
+console.log(other);
---
+++ hw_two/index.js
@@ -0,0 +1,92 @@
+//    ___ Task 1 ___
+console.log('___ Task 1 ___');
+const numbers = [1, 2, 3, 4, 5, 6];
+
+ const someFunction = (numbers) => {
+    return numbers.map(function(number){
+        return number % 2 === 0 ? (number += 1) : (number -= 1);
+    });    
+};
+
+console.log(someFunction(numbers));
+
+ //    ___ Task 2 ___
+console.log(' ___ Task 2 ___');
+const words = ['alex', 'Ovechkin', 'oLeG', 'veCn', 'HoHo'];
+
+const capitalizeFunction = (arrayWords) => {
+    return words.map(function(word){
+        return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    });
+};
+
+console.log(capitalizeFunction(words));
+
+//    ___ Task 3 ___
+console.log(' ___ Task 3 ___ - НЕ ВЫПОЛНЕНО');
+
+const users = [
+    {
+        name: 'Alex',
+        id: 1,
+    },
+    {
+        name: 'Tomara',
+        id: 2,
+    },
+    {
+        name: 'Max',
+        id: 3,
+    },
+];
+const removeFunction = (users) => {
+    return users.delete((user) =>{
+        return users.delete();
+    })
+}
+console.log(removeFunction());
+
+
+//    ___ Task 4 ___
+console.log(' ___ Task 4 ___');
+
+const polindromFunction = (str) => {
+    str = str.toLowerCase();
+    return str === str.split('').reverse().join('');
+}
+console.log(polindromFunction('Anna'))
+console.log(polindromFunction('Hello'))
+console.log(polindromFunction('ШалаШ'))
+
+
+//    ___ Task 5 ___
+console.log(' ___ Task 5 ___ НЕ СМОГ ВЫПОЛНИТЬ');
+
+const customers = [
+    {
+        id: 1,
+        first_name: 'Jeanette',
+        last_name: 'Penddreth',
+        email: 'jpenddreth0@census.gov',
+        gender: 'Female',
+        ip_address: '26.58.193.2',
+    },
+    {
+        id: 2,
+        first_name: 'Petr',
+        last_name: 'Jackson',
+        email: 'gfrediani1@senate.gov',
+        gender: 'Male',
+        ip_address: '229.179.4.212',
+    },
+];
+const someFunc = (customers) => {
+    return customers[first_name].concat([last_name]);
+}
+console.log(someFunc(users))
+
+//    ___ Task 6 ___
+console.log(' ___ Task 6 ___ НЕ ПОНЯЛ ЗАДАНИЯ');
+
+//    ___ Task 7 ___
+console.log(' ___ Task 7 ___ НЕ СМОГ ВЫПОЛНИТЬ');
