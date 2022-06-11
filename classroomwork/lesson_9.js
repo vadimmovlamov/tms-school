78711b81156b00e8501e360d12e05cea876e80d9
vadimmovlamov <vadim113512@tut.by>
2022-06-11 16:34:57 +0300

    add classwork

--- classroomwork/lesson_9.js
+++ classroomwork/lesson_9.js
@@ -9,6 +9,20 @@ const someFun = (numbers) => numbers.reduce((acc, element) => {
 
     return acc;;
 
-}, {even: 0, odd: 0})
+}, {
+    even: 0,
+    odd: 0
+})
 
-console.log(someFun([12,3,4,5,4]));
+console.log(someFun([12, 3, 4, 5, 4]));
+
+
+function Rabbit(name) {
+    this.name = name;
+}
+
+let rabbit = new Rabbit("White Rabbit");
+
+console.log(rabbit.constructor === Rabbit)
+
+let rabbit2 = new rabbit.constructor("Black Rabbit");
