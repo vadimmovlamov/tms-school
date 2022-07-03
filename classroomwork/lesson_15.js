443b7faccb04feb109442b7c33dc084ab628f84f
vadimmovlamov <vadim113512@tut.by>
2022-07-03 22:23:44 +0300

    add a new tasks

--- classroomwork/lesson_15.js
+++ classroomwork/lesson_15.js
@@ -5,41 +5,13 @@ const arrayOfArrays = [
 ];
 
 const bigSum = (arrays) => {
-    return arrays.reduce((result, subArr) => {
-        const sumArr = subArr.reduce((sum, number) => sum + number, 0)
+    return arrays.reduce((result, subArr) => { // reduce мы проходимся по кажому элементу основного массива, то есть по 3-м элементам
+        const sumArr = subArr.reduce((sum, number) => sum + number, 0) // считаем сумму каждого элемента (кажого массива)
         return sumArr > result ? sumArr : result;
     }, 0)
 }
 
 console.log(bigSum(arrayOfArrays));
-
-
-
-/* debounce */
-
-// const debounce = (callback, delay = 1000) => {
-//     let timer;
-
-//     return () => { // каждое нажатие он сбрасывает таймер и запускается все заново
-//         const fnCall = () => callback.apply(this, arguments)
-
-//         clearTimeout(timer);
-
-//         timer = setTimeout(() => {
-//             callback();
-//         }, delay)
-//     }
-// }
-
-// const button = document.querySelector('button'); //сделать кнопку в html
-
-// const callback = (event) => {
-//     console.log(event.target);
-// }
-
-// const debounced = debounce(callback);
-
-// button.addEventListener('click', debounced);
 console.log('');
 
 console.log('вычисление факториала');
@@ -65,9 +37,9 @@ console.log('');
 
 /* https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/train/javascript */
 /* 
-* 'abc' =>  ['ab', 'c_']
-* 'abcdef' => ['ab', 'cd', 'ef']
-*/
+ * 'abc' =>  ['ab', 'c_']
+ * 'abcdef' => ['ab', 'cd', 'ef']
+ */
 const getPairs = (str) => {
 
     const result = []
@@ -82,6 +54,68 @@ const getPairs = (str) => {
     return result;
 }
 console.log(getPairs('abcdlej'));
+console.log('');
 
 
-/*  */
+// способ Адриана
+
+const initialData = 'abcdlej';
+
+const solution = (string) => {
+    const result = []
+
+    for (let i = 0; i < string.length; i += 2) {
+        const currentLetter = string[i]
+        const nextLetter = string[i + 1] || '_'
+
+        // далее мы смотрим, есть ли у нас следующая буква, если она есть то идем дальше, если ее нет то это означает что мы находимся на самой посл букве
+
+        if (nextLetter) {
+            result.push(`${currentLetter}${nextLetter}`)
+        } else {
+            result.push(currentLetter)
+        }
+
+        // вместе условия IF else можно применить push в одну строчку
+        // result.push(`${currentLetter}${nextLetter}`)
+    }
+    return result;
+}
+console.log(solution(initialData));
+console.log('');
+
+
+
+/*_____  debounce  _______*/
+console.log('_____  debounce  _______');
+
+const debounce = (callback, delay = 1000) => { //delay = 1000 здесь 1000 установили по умолчанию
+    // внутри себя функция debounce объявляет переменную timer
+    let timer;
+
+    return function () { // каждое нажатие он сбрасывает таймер и запускается все заново
+        const fnCall = () => callback.apply(this, arguments) //перед тем как запустить fnCall мы очищаем таймер, а затем заново присваиваем задержку в 1000
+
+        clearTimeout(timer); //очищаем таймер после каждого нажатия, т.е каждое нажатие он сбрасывает таймер обратно
+
+        timer = setTimeout(fnCall, delay)// занаво устанавливаем таймер после каждого нажатия
+        // или
+        /* 
+        timer = setTimeout(() => {        // занаво устанавливаем таймер после каждого нажатия
+            callback();
+        }, delay)
+         */
+    }
+}
+
+const button = document.querySelector('#button'); //сделать кнопку в html
+
+const callback = (event) => {
+    console.log('hello');
+    console.log(event.target);
+}
+
+const debounced = debounce(callback);
+
+button.addEventListener('click', debounced);
+console.log('');
--- codewars/task_1.js
+++ codewars/task_1.js
@@ -1,4 +1,6 @@
 /*  */
+console.log('task 1');
+
 const arr1 = [1, 3, 5, 7, 9, 11, 12];
 const arr2 = [1, 2, 3, 4, 5, 10, 1];
 
@@ -13,15 +15,22 @@ const someFunction = (element) => {
 
 someFunction()
 
+console.log('');
+
 /*  */
+console.log('task 2');
+
 const someWords = (word) => ((word.length % 2 === 0) ? word.substring(word.length / 2 - 1, word.length / 2 + 1) : word.charAt(Math.floor(word.length / 2)))
 
 console.log(someWords('test'));
 console.log(someWords('testing'));
 console.log(someWords('middle'));
 console.log(someWords('A'));
+console.log('');
 
 /*  */
+console.log('task 3');
+
 const numb = 370;
 const narcissistic = (value) => {
     const array = value.toString().split('');
@@ -45,9 +54,10 @@ const narcissistic = (value) => {
 
 console.log(narcissistic('370'));
 console.log(narcissistic('1938'));
+console.log('');
 
-
-/* вывести те слова которые === длинне 4 */
+/*  */
+console.log('task 4 - вывести те слова которые === длинне 4');
 
 const arrayName = ["Ryan", "Kieran", "Jason", "Yous", "Ryan", "Kieran"];
 
@@ -60,9 +70,11 @@ const namefunction = (name) => {
     }, [])
 }
 console.log(namefunction(arrayName));
+console.log('');
 
+/*  */
+console.log('task 5 - задача из кодворс по геомтрической прогрессии');
 
-/* задача из кодворс по геомтрической прогрессии */
 const numbers = [1, 3, 5, 9, 11];
 
 const arifmetic = (number) => {
@@ -80,44 +92,247 @@ const arifmetic = (number) => {
     }
 }
 console.log(arifmetic(numbers));
-
-
-/* 
-перевести из римских в арабские 
-https://www.codewars.com/kata/51b6249c4612257ac0000005/train/javascript
-*/
-
-const romeArray = {
-    I: 1,
-    V: 5,
-    X: 10,
-    L: 50,
-    C: 100,
-    D: 500,
-    M: 1000
-}
-
-
-
-
-
-
 console.log('');
+
+
+
 /* https://www.codewars.com/kata/5b02fa2d86d07558c40000c5/train/javascript  */
-const array = [12, 5, 13]
+console.log('task 6');
+const num = [12, 5, 13]
 
 const pytrip = (num) => {
     num.reduce((acc, elem) => {
-        return acc + elem**2
+        return acc + elem ** 2
     }, 0)
 }
-console.log(pytrip(array));
+console.log(pytrip(num));
 
+console.log('task 7');
 
-
-const mas = [1,2,3,4,5]
+const mas = [1, 2, 3, 4, 5]
 const minMax = mas.reduce((acc, m) => {
-    return 
+    return
 }, [])
 
 console.log(Math.max(...mas));
+console.log('');
+
+
+/* https://www.codewars.com/kata/52597aa56021e91c93000cb0/train/javascript 
+
+moveZeros([false,1,0,1,2,0,1,3,"a"]) // returns[false,1,1,2,1,3,"a",0,0]
+
+*/
+console.log('task 8');
+
+const arrayElements = [false, 1, 0, 1, 2, 0, 1, 3, "a"];
+
+const moveZeros = (arr) => {
+    const arrSymbol = []
+    const arrZeros = []
+
+    arr.map((element) => element === 0 ? arrZeros.push(element) : arrSymbol.push(element))
+
+    return [...arrSymbol, ...arrZeros]
+}
+
+console.log(moveZeros(arrayElements));
+console.log('');
+/* другие варианты решения задачи */
+
+/*
+// 1 вараинт 
+var moveZeros = function (arr) {
+    for (var i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] === 0) {
+            arr.splice(i, 1);
+            arr.push(0);
+        }
+    }
+    return arr;
+} 
+
+// 2 вараинт 
+var moveZeros = function (arr) {
+    return arr.filter(x => x !== 0).concat(arr.filter(x => x === 0));
+}
+
+// 3 вараинт 
+var moveZeros = function (arr) {
+    return [...arr.filter(v => v !== 0), ...arr.filter(v => v === 0)]
+}
+*/
+
+
+
+// https://www.codewars.com/kata/52c31f8e6605bcc646000082/train/javascript
+// twoSum([1, 2, 3], 4) // returns [0, 2] or [2, 0]
+console.log('task 9');
+
+const arrayNum = ([1, 2, 3], 4)
+
+function twoSum(numbers, target) {
+    for (var i = 0; i < numbers.length - 1; i++) {
+        for (var j = i + 1; j < numbers.length; j++) {
+            if (numbers[i] + numbers[j] === target) return [i, j];
+        }
+    }
+}
+// for (let i = 0; i < item.length; i++) {    
+//     const sum = item[i] + item[i + 1];
+//     if (sum == target) {
+//         return i, sum
+//     }
+
+// }
+
+console.log(twoSum(arrayNum));
+console.log('');
+
+/* https://www.codewars.com/kata/554e4a2f232cdd87d9000038/javascript */
+/* 
+"ATTGC" --> "TAACG"
+"GTAT" --> "CATA"
+*/
+
+console.log('task 10');
+
+// 1 вариант
+console.log('__ 1 вариант ___');
+const replaceLetter = (str) => (str.split('').map((item) => {
+    if (item == 'A') {
+        return 'T'
+    }
+    if (item == 'T') {
+        return 'A'
+    }
+    if (item == 'C') {
+        return 'G'
+    }
+    if (item == 'G') {
+        return 'C'
+    }
+}).join(''))
+console.log(replaceLetter('ATTGC'));
+console.log(replaceLetter('GTAT'));
+console.log('');
+
+// 2 вариант
+console.log('__ 2 вариант ___');
+
+const replaceLetters = str => str.split('').map(item => item === 'A' ? 'T' : item === 'T' ? 'A' : item === 'G' ? 'C' : item === 'C' ? 'G' : '').join('');
+
+console.log(replaceLetters('ATTGC'));
+console.log(replaceLetters('GTAT'));
+console.log('');
+
+// 3 вариант
+console.log('__ 3 вариант ___');
+
+function DNAStrand(dna) {
+    return dna.replace(/A/g, 't').replace(/T/g, 'a').replace(/C/g, 'g').replace(/G/g, 'c').toUpperCase();
+    //?? почему тут применяется upper и почему сразу буквы в нижнем регистре
+}
+console.log(DNAStrand('ATTGC'));
+console.log(DNAStrand('GTAT'));
+console.log('');
+
+
+/* https://www.codewars.com/kata/596d2447c4a959794d000007/train/javascript */
+/* 
+Return the sum of all even Fibonacci numbers up to and including n.
+Character limit: max. 76 characters (including)
+*/
+
+console.log('task 11');
+
+function sumAllEvenFibonacciNumbers(n) {
+    return n <= 1 ? n : sumAllEvenFibonacciNumbers(n - 1) + sumAllEvenFibonacciNumbers(n - 2)
+}
+
+// console.log(sumAllEvenFibonacciNumbers(40));
+
+
+function fib(n) {
+    if (n < 2) {
+        return 1
+    }
+    return fib(n - 1) + fib(n - 2) // почему тут делаем именно так fib()
+}
+// console.log(fib(40));
+
+
+function fl(n) {
+    let a1 = 0
+    let a2 = 1
+    let curr = 1
+
+    for (let i = 1; i <= n; i++) {
+        curr = a1 + a2
+        a1 = a2
+        a2 = curr
+    }
+    return curr;
+}
+
+// console.log(fl(10));
+console.log('');
+
+
+
+
+
+
+/* 
+https://www.codewars.com/kata/523f5d21c841566fde000009/train/javascript
+
+arrayDiff([1,2],[1]) == [2]
+arrayDiff([1,2,2,2,3],[2]) == [1,3]
+*/
+
+console.log('task 12');
+
+function arrayDiff(a, b) {
+    // 1 вариант
+    /* return a.filter((num) => !b.includes(num)) */
+    
+    // 2 вариант
+    /* return a.map((num) => {
+        return !b.includes(num)
+    }) */
+
+    // 3 вариант
+    let result = []
+
+    for (let i = 0; i < a.length; i++) {
+        // Only push elements from a that aren't in b
+        if (!b.includes(a[i])) {
+            result.push(a[i])
+        }
+    }
+    return result
+}
+
+console.log(arrayDiff([1, 2], [1]));
+console.log(arrayDiff([1, 2, 2, 2, 3], [2]));
+console.log('');
+
+
+/* 
+https://www.codewars.com/kata/56541980fa08ab47a0000040/train/javascript
+*/
+console.log('task 13');
+
+function printerError(s) {
+    const array = s.split('')
+    console.log(array);
+
+    const count = 0
+    for (let i = 0; i < array.length; i++) {
+        count++
+        
+    }
+}
+
+console.log(printerError("aaabbbbhaijjjm"));
+console.log(printerError("aaaxbbbbyyhwawiwjjjwwm"));
